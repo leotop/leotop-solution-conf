6c6cd52d7d2df3a473445284bf2c6052ab3596b9
Igor <specialist-seo@yandex.com>
2014-12-26 10:25:40 +0300

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,4 +1,4 @@
-apt-get install apache2 mysql-server mysql-client php5 php5-gd php-gettext php5-mcrypt php5-mysql mc nano
+apt-get install apache2 mysql-server mysql-client php5 php5-gd php-gettext php5-mcrypt php5-mysql mc nano -y
 a2enmod rewrite
 php5enmod gd mcrypt mysql json
 service apache2 restart
