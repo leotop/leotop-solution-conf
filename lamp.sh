f8140e5354a980c4922e1a8f5634afab8fc07734
Igor <specialist-seo@yandex.com>
2015-04-01 13:40:52 +0300

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,3 +1,6 @@
+apt-get update
+apt-get dist-upgrade
+
 locale-gen ru_RU.UTF-8
 apt-get install apache2 mysql-server mysql-client php5 php5-gd php-gettext php5-mcrypt php5-mysql mc nano -y
 a2enmod rewrite
