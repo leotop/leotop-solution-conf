2cc671d9108e9168fbbc67e7c95cb85654cdd2cd
Igor <specialist-seo@yandex.com>
2015-04-01 17:33:19 +0300

    Update django-py3.bash

--- django-py3.bash
+++ django-py3.bash
@@ -1,4 +1,4 @@
 apt-get update
 apt-get dist-upgrade
 locale-gen ru_RU.UTF-8
-apt-get install apache2 mc nano libapache2-mod-wsgi-py3 python3-pip -y
+apt-get install apache2 mc nano libapache2-mod-wsgi-py3 python3-pip python3-imaging -y
