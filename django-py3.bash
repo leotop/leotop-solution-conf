b3e26888197922d5c0efade1c68e693641c34f92
Igor <specialist-seo@yandex.com>
2015-04-14 01:25:52 +0300

    Update django-py3.bash

--- django-py3.bash
+++ django-py3.bash
@@ -4,5 +4,4 @@ locale-gen ru_RU.UTF-8
 apt-get install apache2 mc nano libapache2-mod-wsgi-py3 python3-pip python3-imaging -y
 pip3 install django
 pip3 install django-mptt
-pip3 install django-ckeditor
 mkdir /var/www/log
