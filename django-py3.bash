c75cf55f48ca8dbfba5eb65caf6ccf98d61ef420
Igor <specialist-seo@yandex.com>
2015-04-27 16:49:28 +0300

    add mysql driver

--- django-py3.bash
+++ django-py3.bash
@@ -6,3 +6,7 @@ apt-get install apache2 mc nano libapache2-mod-wsgi-py3 python3-pip python3-imag
 pip3 install django
 pip3 install django-mptt
 mkdir /var/www/log
+
+#mysql python3
+apt-get install python3-dev libmysqlclient-dev
+pip3 install mysqlclient
