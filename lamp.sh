484659ce5c6e89d454f71bf92ad365507b32b2f1
Igor <specialist-seo@yandex.com>
2015-05-14 10:42:07 +0300

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -2,7 +2,7 @@ apt-get update
 apt-get dist-upgrade
 
 locale-gen ru_RU.UTF-8
-apt-get install dialog apache2 mysql-server mysql-client php5 php5-gd php5-curl curl php-gettext php5-mcrypt php5-mysql mc nano -y
+apt-get install dialog apache2 mysql-server mysql-client php5 php5-gd php5-curl curl php-gettext php5-mcrypt php5-mysql mc nano phpmyadmin -y
 a2enmod rewrite
 php5enmod gd mcrypt mysql json curl
 service apache2 restart
