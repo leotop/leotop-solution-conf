80e16c325fe86a81c1708afc56249810ebb51f3b
Igor <specialist-seo@yandex.com>
2015-10-26 02:17:47 +0300

    Update django-py3.bash

--- django-py3.bash
+++ django-py3.bash
@@ -5,6 +5,7 @@ apt-get install dialog -y
 apt-get install apache2 mc nano libapache2-mod-wsgi-py3 python3-pip python3-imaging -y
 pip3 install django
 pip3 install django-mptt
+pip3 install django-taggit
 mkdir /var/www/log
 
 #mysql python3
