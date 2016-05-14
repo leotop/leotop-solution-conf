31e85b3526172e876d3f1cbd9abaf4a5e434ba15
Igor <specialist-seo@yandex.com>
2016-05-14 21:29:53 +0400

    python3-pil new, python3-imagins old
    python3-pil new, python3-imagins old

--- django-py3.bash
+++ django-py3.bash
@@ -2,7 +2,7 @@ apt-get update
 apt-get dist-upgrade
 locale-gen ru_RU.UTF-8
 apt-get install dialog -y
-apt-get install apache2 mc nano libapache2-mod-wsgi-py3 python3-pip python3-imaging -y
+apt-get install apache2 mc nano libapache2-mod-wsgi-py3 python3-pip python3-pil -y
 pip3 install django
 pip3 install django-mptt
 pip3 install django-taggit
