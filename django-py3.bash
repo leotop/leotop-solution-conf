b48dcd0ec4ab4b799edb1c20e45b5d8bc4f9fe1f
Igor <specialist-seo@yandex.com>
2016-05-14 23:30:49 +0400

    Update django-py3.bash

--- django-py3.bash
+++ django-py3.bash
@@ -3,9 +3,13 @@ apt-get dist-upgrade
 locale-gen ru_RU.UTF-8
 apt-get install dialog -y
 apt-get install apache2 mc nano libapache2-mod-wsgi-py3 python3-pip python3-pil -y
+
+# django
 pip3 install django
 pip3 install django-mptt
 pip3 install django-taggit
+pip3 install django-import-export 
+pip3 installfeedparser
 
 #mysql python3
 apt-get install python3-dev libmysqlclient-dev
